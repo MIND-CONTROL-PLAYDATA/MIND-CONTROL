b638e156efcd87780b03dbffddca0117374181f4
icebear2n2 <icebear2n2@gmail.com>
2023-07-18 17:35:06 +0900

    init: 프로젝트 초기 설정

---
+++ mindControl/src/test/java/com/playdata/mindcontrol/MindControlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.playdata.mindcontrol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MindControlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
